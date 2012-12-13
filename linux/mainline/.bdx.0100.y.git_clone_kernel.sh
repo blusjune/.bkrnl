c1a1a58aa9fa1d942a4ab453edf5e482acbe49b1
Brian Myungjune JUNG <blusjune+github@gmail.com>
2012-12-14 01:45:16 +0900

    linux-stable source tree added

--- linux/mainline/.bdx.0100.y.git_clone_kernel.sh
+++ linux/mainline/.bdx.0100.y.git_clone_kernel.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+
 ##.bdx.0100.y.git_clone_kernel.sh
 ##_tstamp="20121208_020758";
+##_ver=20121213_233950
 
-echo "#>> git-clone: torvalds mainline kernel";
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+
+#echo "#>> git-clone: torvalds mainline kernel";
+#git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+
+
+echo "#>> git-clone: linux stable kernel";
+git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
