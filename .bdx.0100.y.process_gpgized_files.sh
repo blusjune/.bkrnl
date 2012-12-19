c9f5ed567b81803e0cf11889bb1abeeb1a767fc8
Brian Myungjune JUNG <blusjune+github@gmail.com>
2012-12-19 13:21:11 +0900

    .bdx modified

--- .bdx.0100.y.process_gpgized_files.sh
+++ .bdx.0100.y.process_gpgized_files.sh
@@ -8,8 +8,8 @@ echo "#>> update gighub source tree (add-commit-push)";
 rm -f .tstamp.*;
 touch .tstamp.$(tstamp);
 
-#git add -A
-git add .tstamp.*
+git add -A
+#git add .tstamp.*
 git commit -a
 git push -u
 
