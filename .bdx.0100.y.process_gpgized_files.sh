733cd2b1de4e3fa56ed2f9459f432075dc997af8
Brian Myungjune JUNG <blusjune+github@gmail.com>
2013-08-03 10:19:00 +0900

    branch bsos-v0.1.0 is merged to master;

--- .bdx.0100.y.process_gpgized_files.sh
+++ .bdx.0100.y.process_gpgized_files.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#.bdx.0100.y.process_gpgized_files.sh
+#_ver="20130803_101736";
 
 
 
@@ -11,6 +13,11 @@ touch .tstamp.$(tstamp);
 git add -A
 #git add .tstamp.*
 git commit -a
+read -p "#<< Go ahead? [Y|n] " _ans;
+if [ "X$_ans" = "Xn" ]; then
+	echo "#>> Exit without pushing";
+	exit 0;
+fi
 git push --all -u
 
 
